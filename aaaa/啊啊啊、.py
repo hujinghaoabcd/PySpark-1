93130f31df5f0ccb453df066bafd2ee147b9a695
huashishaojie <1353560632@qq.com>
2018-09-28 12:30:29 +0800

    Initial commit

---
+++ aaaa/啊啊啊、.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+import seaborn as sns
+sns.set()
+from sklearn.datasets.samples_generator import make_blobs
+x,y =make_blobs(n_samples=50,centers=2,
+                random_state=0,cluster_std=0.60)
+plt.scatter(x[:,0],x[:,1],c=y,s=50,cmap='spring')
+xfit=np.linspace(-1,3.5)
+plt.scatter(x[:,0],x[:,1],c=y,s=50,cmap='spring')
+
+for m,b in [(1,0.65),(0.5,1.6),(-0.2,2.9)]:
+    plt.plot(xfit,m * xfit + b, '-k')
+plt.xlim(-1,3.5)
